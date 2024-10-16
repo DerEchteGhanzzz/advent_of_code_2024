a530b19abd37af98dd136326e1ea676e578aae0c
Blanke Stijn <blankensteijnhans@gmail.com>
2024-10-16 14:28:08 +0200

    hello world

---
+++ target/debug/deps/avent_of_code_2024-c39cf544f2b7ef89.d
@@ -0,0 +1,5 @@
+C:\Users\Hans\CodingProjects\personal\AOC\avent_of_code_2024\target\debug\deps\libavent_of_code_2024-c39cf544f2b7ef89.rmeta: src/main.rs
+
+C:\Users\Hans\CodingProjects\personal\AOC\avent_of_code_2024\target\debug\deps\avent_of_code_2024-c39cf544f2b7ef89.d: src/main.rs
+
+src/main.rs:
